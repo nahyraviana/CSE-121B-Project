42962c283d2fa441c716fc45c1773e4a7db1fed1
nahyraviana <nahyraviana@gmail.com>
2022-02-13 00:56:43 -0700

    up

--- main.js
+++ main.js
@@ -24,33 +24,12 @@ function getResults(query) {
         .then(weather => { return weather.json(); })
         .then(displayResults);
 }
-
-/*STEP 5: Create a function to display the results*/
-
-function displayResults(weather) {
-    console.log(weather);
-    /*STEP 6: assign the properties to a variable and display it*/
-    let city = document.querySelector(".location .city");
-    city.innerText = `${weather.name} , ${weather.sys.country}`;
-    /*STEP 7: Declare and initialize a new variable to hold the current date*/
-    let now = new Date();
-    /* STEP 8: Assign the date property to a variable and call the current date*/
-    let date = document.querySelector(".location .date");
-    date.innerText = dateBuilder(now);
-
-    let temp = document.querySelector(".current .temp");
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
-
-    let weatherElement = document.querySelector(".current .weather");
-    weatherElement.innerText = weather.weather[0].main;
-    let hilo = document.querySelector(".hi-lo");
-    hilo.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
-}
-
+/*STEP 5: Create a function to build the date and return the display of Day, date, month, year*/
 function dateBuilder(d) {
+    /* STEP 6: Declare an array to store the months of the year and another to the days of the week*/
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+    /*STEP 7: Use built in methods to call the day,date,month and full year*/
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
@@ -58,4 +37,28 @@ function dateBuilder(d) {
     return `${day} ${date} ${month} ${year}`;
 
 
+}
+/*STEP 8: Create a function to display the results*/
+
+function displayResults(weather) {
+    console.log(weather);
+    /*STEP 9: assign the properties to a variable and display it*/
+    let city = document.querySelector(".location .city");
+    city.innerText = `${weather.name} , ${weather.sys.country}`;
+    /*STEP 10: Declare and initialize a new variable to hold the current date*/
+    let now = new Date();
+    /* STEP 11: Assign the date property to a variable and call the current date*/
+    let date = document.querySelector(".location .date");
+    date.innerText = dateBuilder(now);
+    /*STEP 12: Assign the temp property to a variable and display the current temperature */
+    let temp = document.querySelector(".current .temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
+
+    /*STEP 12: Assign the weather property to a variable and display the current weather */
+    let weatherElement = document.querySelector(".current .weather");
+    weatherElement.innerText = weather.weather[0].main;
+
+    /*STEP 13: Assign the hi-lo property to a variable and display the Max and min temperatures */
+    let hilo = document.querySelector(".hi-lo");
+    hilo.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
 }
